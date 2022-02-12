0d39a2ffcff759683099a6e39b6256944d20d00e
Diego Adami Biancardi <diegoadamibiancardi@gmail.com>
2022-02-12 19:15:16 -0300

    Teste do método soma - ok

--- src/application/Teste_Tabuada.java
+++ src/application/Teste_Tabuada.java
@@ -10,7 +10,16 @@ public class Teste_Tabuada {
 		SecureRandom geradorDeNumeros = new SecureRandom();
 		Scanner input = new Scanner(System.in);
 		
-		
+		int x = geradorDeNumeros.nextInt(10);
+		int y = geradorDeNumeros.nextInt(10);
+		System.out.print(x + " + " + y + " = ");
+		int resultado = input.nextInt();
+		if(resultado == soma(x, y)) {
+			System.out.println("Muito bem!");
+		}
+		else {
+			System.out.println("Errado!");
+		}
 		
 		input.close();
 
